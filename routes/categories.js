3a7ca0cd005a4e2a25aaa3e559ba3670c937fc1e
Joseph Nasser <josephjaonasser@gmail.com>
2023-01-24 22:22:21 +0800

    pass store id and access token as params

--- routes/categories.js
+++ routes/categories.js
@@ -1,12 +1,17 @@
 const express = require("express");
-const api = require("../helpers/ecwidAPI");
+const EcwidApiHelper = require("../helpers/EcwidApiHelper");
 const router = express.Router();
 
 /***
  * @method get
  * @desc get all categories
  */
-router.get("/api/categories", async (req, res) => {
+router.get("/api/:storeId/categories", async (req, res) => {
+  const store_id = req.params.storeId;
+  const { authorization } = req.headers;
+
+  const api = new EcwidApiHelper(store_id, authorization);
+
   try {
     const categories = await api.get("/categories");
 
@@ -23,7 +28,12 @@ router.get("/api/categories", async (req, res) => {
  * @method get
  * @desc get categories by id
  */
-router.get("/api/categories/:id", async (req, res) => {
+router.get("/api/:storeId/categories/:id", async (req, res) => {
+  const store_id = req.params.storeId;
+  const { authorization } = req.headers;
+
+  const api = new EcwidApiHelper(store_id, authorization);
+
   try {
     const category = await api.get("/categories", id);
 
@@ -36,10 +46,10 @@ router.get("/api/categories/:id", async (req, res) => {
   }
 });
 
-router.post("/api/categories", async (req, res) => {});
+router.post("/api/:storeId/categories", async (req, res) => {});
 
-router.patch("/api/categories/:id", async (req, res) => {});
+router.patch("/api/:storeId/categories/:id", async (req, res) => {});
 
-router.delete("/api/categories/:id", async (req, res) => {});
+router.delete("/api/:storeId/categories/:id", async (req, res) => {});
 
 module.exports = router;
--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,18 @@
+const e = require("express");
 const express = require("express");
-const api = require("../helpers/ecwidAPI");
+const EcwidApiHelper = require("../helpers/EcwidApiHelper");
 const router = express.Router();
 
 /***
  * @method get
  * @desc get all products
  */
-router.get("/api/products", async (req, res) => {
+router.get("/api/:storeId/products", async (req, res) => {
+  const store_id = req.params.storeId;
+  const { authorization } = req.headers;
+
+  const api = new EcwidApiHelper(store_id, authorization);
+
   try {
     const products = await api.get("/products");
 
@@ -23,8 +29,10 @@ router.get("/api/products", async (req, res) => {
  * @method get
  * @desc search for a product by keyword/s
  */
-router.get("/api/products/search", async (req, res) => {
-  const { keyword } = req.query;
+router.get("/api/:storeId/products/search", async (req, res) => {
+  const store_id = req.params.storeId;
+  const { authorization } = req.headers;
+  const api = new EcwidApiHelper(store_id, authorization);
 
   try {
     const product = await api.get("/products?keyword=" + keyword);
@@ -42,29 +50,76 @@ router.get("/api/products/search", async (req, res) => {
  * @method get
  * @desc filter products (see ecwid api docs)
  */
-router.get("/api/products/filter", async (req, res) => {
-  const queryKeys = [
-    "priceFrom",
-    "priceTo",
-    "categories",
-    "includeProductsFromSubcategories",
-    "sortBy",
-    "limit",
-    "offset",
-  ];
+router.get("/api/:storeId/products/filter", async (req, res) => {
+  const store_id = req.params.storeId;
+  const { authorization } = req.headers;
+  const api = new EcwidApiHelper(store_id, authorization);
 
-  let route = "/products";
+  // const queryKeys = [
+  //   "priceFrom",
+  //   "priceTo",
+  //   "categories",
+  //   "includeProductsFromSubcategories",
+  //   "sortBy",
+  //   "limit",
+  //   "offset",
+  //   "externalReferenceId",
+  //   "createdFrom",
+  //   "createdTo",
+  //   "updatedFrom",
+  //   "updatedTo",
+  //   "enabled",
+  //   "sku",
+  //   "productId",
+  //   "isGiftCard",
+  //   "discountsAllowed",
+  //   "baseUrl",
+  //   "cleanUrls",
+  //   "onsale",
+  //   "inventory",
+  //   "isCustomerSetPrice",
+  //   "visibleInStorefront",
+  //   "option_",
+  //   "attribute_",
+  // ];
 
-  for (let index in queryKeys) {
-    const key = queryKeys[index];
-    if (req.query[key]) {
-      route += route.includes("?") ? "&" : "?";
-      route += key + "=" + req.query[key].replace(" ", "%20");
-    }
-  }
+  // let route = "/products";
+
+  // for (let index in queryKeys) {
+  //   const key = queryKeys[index];
+  //   console.log(key);
+  //   if (req.query[key]) {
+  //     route += route.includes("?") ? "&" : "?";
+  //     route += key + "=" + req.query[key].replace(" ", "%20");
+  //   }
+
+  //   if (key === "option_") {
+  //     const optionKeys = Object.keys(req.query).filter(
+  //       (key) => key.indexOf("option_") === 0
+  //     );
+
+  //     optionKeys.forEach((optKey) => {
+  //       route += route.includes("?") ? "&" : "?";
+  //       route += optKey + "=" + req.query[optKey].replace(" ", "%20");
+  //     });
+  //   }
+
+  //   if (key === "attribute_") {
+  //     const attribKeys = Object.keys(req.query).filter(
+  //       (key) => key.indexOf("attribute_") === 0
+  //     );
+
+  //     attribKeys.forEach((attribKey) => {
+  //       route += route.includes("?") ? "&" : "?";
+  //       route += attribKey + "=" + req.query[attribKey].replace(" ", "%20");
+  //     });
+  //   }
+  // }
+
+  const searchParams = new URLSearchParams(req.query).toString();
 
   try {
-    const products = await api.get(route);
+    const products = await api.get("/products?" + searchParams);
 
     return res.status(200).send(products.data);
   } catch (e) {
@@ -79,8 +134,12 @@ router.get("/api/products/filter", async (req, res) => {
  * @method get
  * @desc get a product by id
  */
-router.get("/api/products/:id", async (req, res) => {
-  const { id } = params.id;
+router.get("/api/:storeId/products/:id", async (req, res) => {
+  const id = req.params.id;
+  const store_id = req.params.storeId;
+  const { authorization } = req.headers;
+  const api = new EcwidApiHelper(store_id, authorization);
+
   try {
     const product = await api.get("/products", id);
 
@@ -93,10 +152,10 @@ router.get("/api/products/:id", async (req, res) => {
   }
 });
 
-router.post("/api/products", async (req, res) => {});
+router.post("/api/:storeId/products", async (req, res) => {});
 
-router.patch("/api/products/:id", async (req, res) => {});
+router.patch("/api/:storeId/products/:id", async (req, res) => {});
 
-router.delete("/api/products/:id", async (req, res) => {});
+router.delete("/api/:storeId/products/:id", async (req, res) => {});
 
 module.exports = router;
