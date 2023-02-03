258465831889e753afd9ca014b51e608365bf046
Joseph Nasser <josephjaonasser@gmail.com>
2023-02-03 15:53:43 +0800

    add product translator

--- lib/translationKit/ecwid.ts
+++ lib/translationKit/ecwid.ts
@@ -1,14 +1,85 @@
 import EcwidProductType from "../types/ecwid/EcwidProductType";
 import EcwidCategoryType from "../types/ecwid/EcwidCategoryType";
 import EcwidMultiItemResponse from "../types/ecwid/EcwidMultiItemResponse";
+import StandardizedProduct, {
+  FileAttatchments,
+} from "../types/StandardizedProduct";
+import { Image } from "../types/generalTypes";
 
 const EcwidTranslator = {
   Product: {
-    translateSingle: (rawData) => {},
-    translateMultiple: (rawData) => {},
+    /**
+     * @description accepts a single product from ecwid and converts it into the standardized format
+     * @param rawData - raw data from the ecwid product
+     * @returns standardized product
+     */
+    translateSingle: (rawData: EcwidProductType): StandardizedProduct => {
+      const standardizedProduct: StandardizedProduct = {
+        id: rawData.id,
+        sku: rawData.sku,
+        name: rawData.name,
+        url: rawData.url,
+        description: rawData.description,
+        translations: {
+          name: rawData.nameTranslated,
+          description: rawData.descriptionTranslated,
+        },
+        short_description: "",
+        current_price: rawData.defaultDisplayedPrice,
+        regular_price: rawData.price,
+        sale_price: rawData.compareToPriceDiscount,
+        price_formatted: rawData.compareToPriceFormatted,
+        in_stock: rawData.inStock,
+        weight: rawData.weight,
+        dimensions: rawData.dimensions,
+        date_created: new Date(rawData.createTimestamp),
+        date_modified: new Date(rawData.updateTimestamp),
+        related_product_ids: rawData.relatedProducts.productIds,
+        quantity: rawData.quantity,
+        categories: rawData.categoryIds,
+        fileAttatchments: rawData.files?.map(
+          (file): FileAttatchments => ({
+            id: file.id,
+            name: file.name,
+            url: file.adminUrl,
+            description: file.description,
+            size: file.size,
+          })
+        ),
+        images: rawData.media?.images?.map(
+          (image): Image => ({
+            src: image.imageOriginalUrl,
+            alt: "",
+            id: image.id,
+          })
+        ),
+        rawData: rawData as EcwidProductType,
+      };
+
+      return standardizedProduct;
+    },
+
+    /**
+     * @description accepts a multi-product response from ecwid and converts each item into the standardized format
+     * @param rawData - raw data from the ecwid multi-product response
+     * @returns array of standardized products
+     */
+    translateMultiple: (
+      rawData: EcwidMultiItemResponse<EcwidProductType>
+    ): StandardizedProduct[] => {
+      const standardizedProductArr: StandardizedProduct[] = rawData.items.map(
+        (item) => EcwidTranslator.Product.translateSingle(item)
+      );
+
+      return standardizedProductArr;
+    },
   },
   Category: {
-    translateSingle: (rawData) => {},
-    translateMultiple: (rawData) => {}
+    translateSingle: (rawData: EcwidCategoryType) => {},
+    translateMultiple: (
+      rawData: EcwidMultiItemResponse<EcwidCategoryType>
+    ) => {},
   },
 };
+
+export default EcwidTranslator;
---
+++ lib/types/StandardizedProduct.ts
@@ -0,0 +1,44 @@
+import { Image, Translations } from "./generalTypes";
+
+interface ProductBasicData {
+  id: string | number;
+  sku: string;
+  name: string;
+  url: string;
+  description: string;
+  translations: Translations;
+  short_description: string;
+  current_price: number;
+  regular_price: number;
+  sale_price: number;
+  price_formatted: string;
+  in_stock: Boolean;
+  weight: string | number;
+  dimensions: Dimensions;
+  date_created: Date;
+  date_modified: Date;
+  related_product_ids: string[] | number[];
+  quantity: number;
+  categories: number[];
+  fileAttatchments: FileAttatchments[];
+  images: Image[];
+  rawData: any;
+}
+
+interface Dimensions {
+  length: string | number;
+  width: string | number;
+  height: string | number;
+}
+
+export interface FileAttatchments {
+  id: string | number;
+  name?: string;
+  description?: string;
+  url: string;
+  size?: number;
+}
+
+export default interface StandardizedProduct extends ProductBasicData {
+  variations?: ProductBasicData[];
+}
