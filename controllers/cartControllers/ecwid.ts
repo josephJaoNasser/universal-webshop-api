a08b5eb9a50ffbe6dcfd7cc81d09c0eec8465b27
Joseph Nasser <josephjaonasser@gmail.com>
2023-02-08 15:41:03 +0800

    edit some types and parameters

--- controllers/cartControllers/ecwid.ts
+++ controllers/cartControllers/ecwid.ts
@@ -1,8 +1,12 @@
+import EcwidApi from "@/lib/apiKit/Ecwid";
 import { ConvertedOrderPayload } from "@/lib/apiKit/Ecwid/cart";
 import OrderEntry from "@/lib/apiKit/Ecwid/types/Order/Order";
 import { Params } from "..";
 
-type EcwidCartResponse = OrderEntry | ConvertedOrderPayload;
+type EcwidCartResponse =
+  | OrderEntry
+  | ConvertedOrderPayload
+  | { updateCount: number };
 
 export default async function ecwidCartHandler({
   method,
@@ -10,6 +14,7 @@ export default async function ecwidCartHandler({
   storeInfo,
   queries,
   id,
+  payload,
 }: Params): Promise<EcwidCartResponse> {
   if (!credentials.token) {
     throw new Error("No token provided");
@@ -20,6 +25,16 @@ export default async function ecwidCartHandler({
   }
 
   try {
+    const Ecwid = new EcwidApi(storeInfo.storeId as number, credentials.token);
+
+    if (method === "updateCart") {
+      Ecwid.Cart.setCartPayload(payload);
+      const response = await Ecwid.Cart.updateCart();
+      return response;
+    } else {
+      const response: EcwidCartResponse = await Ecwid.Cart[method]({ id });
+      return response;
+    }
   } catch (e) {
     throw e;
   }
