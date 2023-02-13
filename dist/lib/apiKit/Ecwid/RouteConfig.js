daa672bf8b25a607de8fd090019b37cee38125f9
Joseph Nasser <57519561+josephJaoNasser@users.noreply.github.com>
2023-02-13 21:23:29 +0800

    delete and recompile dist folder

--- dist/lib/apiKit/Ecwid/RouteConfig.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class RouteConfig {
-    constructor(baseURL, config) {
-        this.baseURL = baseURL;
-        this.config = config;
-    }
-}
-exports.default = RouteConfig;
